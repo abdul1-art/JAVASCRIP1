4df433427b55f3547b66f544867dbc26dc77f162
abdul1-art <abdul1125@student.abudzar.sch.id>
2026-02-12 11:20:20 +0700

    tugas-31

---
+++ pertemuan-6/konversi-type.js
@@ -0,0 +1,42 @@
+// ubah tipe data string ke number
+
+// konverrsi string
+let age = 20;
+
+console.log(age);
+console.log(typeof age);
+
+// konvert
+let convert = String(age);
+console.log(convert);
+console.log(typeof convert);
+
+
+console.log(typeof age);
+age = String(age);
+console.log(typeof age);
+
+// konversi string ke number
+let angka = "10";
+console.log(angka);
+console.log(typeof angka);
+let convertAngka = Number(angka);
+console.log(convertAngka);
+console.log(typeof convertAngka);
+
+// konversi bolean 
+let value1 = 0;
+let value2 = 1;
+let value3 = "";
+let value4 = "hello";
+let value5 = null;  
+let value6 = undefined;
+let value7 = NaN;
+console.log(Boolean(value1)); 
+console.log(Boolean(value2));
+console.log(Boolean(value3));
+console.log(Boolean(value4));
+console.log(Boolean(value5));
+console.log(Boolean(value6));
+console.log(Boolean(value7));
+
---
+++ tugas-31/konversi-data.js
@@ -0,0 +1,12 @@
+// soal 1
+let dressSize = 45;
+dressSize = String(dressSize);
+console.log(typeof dressSize);
+
+dressSize = Number(dressSize);
+console.log(typeof dressSize);
+
+// soal 2
+let isLight = true;
+isLight = String(isLight);
+console.log(typeof isLight);
