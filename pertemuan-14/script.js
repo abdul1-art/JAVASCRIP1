24bc852d090ac14db32d587add5debb33a119cf4
abdul1-art <abdul1125@student.abudzar.sch.id>
2026-05-14 10:06:34 +0700

    tugas-38

---
+++ pertemuan-14/script.js
@@ -0,0 +1,24 @@
+let nama = null;
+let namaPengguna = nama ?? "tidak ada nama";
+console.log(namaPengguna);
+
+
+let firstName = null;
+let lastName = null;
+let nikName= 'asik';
+
+console.log(firstName || lastName || nikName || 'asik');
+
+
+let apple = null
+let grape = null
+let orange = '0';
+let blueberry = '';
+console.log(apple || grape || blueberry || orange);
+
+
+let apple1 = null
+let grape2 = null
+let orange3 = '0';
+let blueberry4 = '';
+console.log(apple1 && grape2 && blueberry4 && orange3);
---
+++ tugas-38/script.js
@@ -0,0 +1,14 @@
+// jawaban no 1
+let apel = null;
+let mangga = "";
+let jeruk = "jeruk ada";
+
+console.log(apel || mangga || jeruk); //*hasilnya adalah = jeruk ada
+
+// jawaban no 2
+let teh = null;
+let kopi = undefined;
+let susu = 10;
+
+console.log(teh ?? kopi ?? susu); //*hasilnya adalah = 10
+
